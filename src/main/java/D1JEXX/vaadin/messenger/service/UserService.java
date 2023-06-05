db5cfe498d1b17070a17aa416fe4acd4381cb797
DIJEXX <113792008+DIJEXX@users.noreply.github.com>
2023-06-06 02:55:35 +0300

    Update UserService.java

--- src/main/java/D1JEXX/vaadin/messenger/service/UserService.java
+++ src/main/java/D1JEXX/vaadin/messenger/service/UserService.java
@@ -1,19 +1,131 @@
 package com.example.messenger.service;
 
-import com.example.messenger.repos.UserRepo;
+
+
+
+
+
+import com.example.messenger.entity.Role;
+
+
+import com.example.messenger.entity.User;
+
+import com.example.messenger.repository.RoleRepository;
+
+import com.example.messenger.repository.UserRepository;
+
 import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.security.core.userdetails.UserDetails;
+
 import org.springframework.security.core.userdetails.UserDetailsService;
+
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
 import org.springframework.stereotype.Service;
 
+import javax.persistence.EntityManager;
+
+import javax.persistence.PersistenceContext;
+
+import java.util.Collections;
+
+import java.util.List;
+
+import java.util.Optional;
+
 @Service
+
 public class UserService implements UserDetailsService {
+
+    @PersistenceContext
+
+    private EntityManager em;
+
     @Autowired
-    private UserRepo userRepo;
+
+    UserRepository userRepository;
+
+    @Autowired
+
+    RoleRepository roleRepository;
+
+    @Autowired
+
+    BCryptPasswordEncoder bCryptPasswordEncoder;
 
     @Override
+
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        return userRepo.findByUsername(username);
+
+        User user = userRepository.findByUsername(username);
+
+        if (user == null) {
+
+            throw new UsernameNotFoundException("User not found");
+
+        }
+
+        return user;
+
     }
+
+    public User findUserById(Long userId) {
+
+        Optional<User> userFromDb = userRepository.findById(userId);
+
+        return userFromDb.orElse(new User());
+
+    }
+
+    public List<User> allUsers() {
+
+        return userRepository.findAll();
+
+    }
+
+    public boolean saveUser(User user) {
+
+        User userFromDB = userRepository.findByUsername(user.getUsername());
+
+        if (userFromDB != null) {
+
+            return false;
+
+        }
+
+        user.setRoles(Collections.singleton(new Role(1L, "ROLE_USER")));
+
+        user.setPassword(bCryptPasswordEncoder.encode(user.getPassword()));
+
+        userRepository.save(user);
+
+        return true;
+
+    }
+
+    public boolean deleteUser(Long userId) {
+
+        if (userRepository.findById(userId).isPresent()) {
+
+            userRepository.deleteById(userId);
+
+            return true;
+
+        }
+
+        return false;
+
+    }
+
+    public List<User> usergtList(Long idMin) {
+
+        return em.createQuery("SELECT u FROM User u WHERE u.id > :paramId", User.class)
+
+                .setParameter("paramId", idMin).getResultList();
+
+    }
+
 }
