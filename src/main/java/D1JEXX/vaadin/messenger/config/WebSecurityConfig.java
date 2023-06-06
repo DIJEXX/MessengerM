db9303652a3684417c8780af24b5054c3de62c66
DIJEXX <113792008+DIJEXX@users.noreply.github.com>
2023-06-06 09:17:57 +0300

    Update WebSecurityConfig.java

--- src/main/java/D1JEXX/vaadin/messenger/config/WebSecurityConfig.java
+++ src/main/java/D1JEXX/vaadin/messenger/config/WebSecurityConfig.java
@@ -35,6 +35,6 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.userDetailsService(userService)
-                .passwordEncoder(NoOpPasswordEncoder.getInstance());
+                .passwordEncoder(BCryptPasswordEncoder.getInstance());
     }
 }
